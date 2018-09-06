688262f262c8b72d0592f1495a6d5c61a3a5d62b
Raghav Gilhotra <gilhotra.raghav71@gmail.com>
2018-09-07 00:01:21 +0530

    Added parents and category name to show poll.

--- app/serializers/poll_serializer.rb
+++ app/serializers/poll_serializer.rb
@@ -6,17 +6,30 @@ class PollSerializer < ActiveModel::Serializer
              :created_at, :multiple_choice, :custom_fields, :poll_option_names,
              :notify_on_participate, :subscribed, :example, :anonymous, :pass_percentage, :stop_percentage,
              :resubmission_active_days, :pass_percentage_drop, :resubmission_count, :poll_category_id,
-             :status, :parent_id, :can_respond_maybe
+             :poll_category_name, :status, :parent_id, :can_respond_maybe
 
   has_one :author, serializer: UserSerializer, root: :users
   has_one :current_outcome, serializer: Full::OutcomeSerializer, root: :outcomes
   has_one :my_stance, serializer: StanceSerializer, root: :stances
   has_one :guest_group, serializer: Simple::GroupSerializer, root: :groups
   has_one :child_poll, serializer: PollSerializer, root: :child_poll
+  has_many :parents, serializer: PollSerializer
   has_many :poll_options, serializer: PollOptionSerializer, root: :poll_options
 
   def subscribed
     object.poll_unsubscriptions.find_by(user: scope[:current_user]).blank?
+  end
+
+  def parents
+    parents = []
+    parents << (parent1 = object.parent_poll) if object.parent_poll
+    parents << (parent2 = parent1.parent_poll) if( parent1 && parent1.parent_poll)
+    parents << (parent3 = parent2.parent_poll) if( parent2 && parent2.parent_poll)
+    parents
+  end
+
+  def poll_category_name
+    object.poll_category.name
   end
 
   def my_stance
