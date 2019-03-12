fa80fb6072e145cb28829014f68793385bf70821
Raghav Gilhotra <gilhotra.raghav71@gmail.com>
2019-03-12 20:39:25 +0530

    Added General to default categories.

--- app/models/formal_group.rb
+++ app/models/formal_group.rb
@@ -182,7 +182,8 @@ class FormalGroup < Group
 
   def create_special_categories
     categories = ["Forge Alliance", "Alliance Decision", "Increase Voting Power",
-                  "Decrease Voting Power", "Invite Member", "Exile Member"]
+                  "Decrease Voting Power", "Invite Member", "Exile Member",
+                  "General"]
     categories.each do |category|
       self.poll_categories.create(name: category, pass_percentage: 70,
                                   stop_percentage: 30, active_days: 5, resubmission_active_days: 3,
